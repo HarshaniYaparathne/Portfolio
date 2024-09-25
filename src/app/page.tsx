4b884b2346bf7b4608c814f79197b5086a7b7c40
HarshaniYaparathne <ymhdyaparathne@std.appsc.sab.ac.lk>
2024-09-25 10:35:40 +0530

    Added new files

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import MySection from "./components/pages/MySection";
+
+export default function Home() {
+  return (
+    <div>
+      <MySection/>
+    </div>
+  );
+}
