e6749bdd5c8e00afd18e5e92ddf8ca07c4439520
HarshaniYaparathne <ymhdyaparathne@std.appsc.sab.ac.lk>
2024-09-28 12:02:13 +0530

    Change navbar color

--- src/app/components/common/NavBar.tsx
+++ src/app/components/common/NavBar.tsx
@@ -4,21 +4,30 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="bg-gray-900 p-4 w-full fixed top-0 z-50">
+    <nav className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 p-4 w-full fixed top-0 z-50 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
-        <ul className="flex space-x-6">
+        <ul className="flex space-x-8">
           <li>
-            <Link href="/#about" className="text-white hover:text-gray-400">
-              AboutMe
+            <Link
+              href="/#about"
+              className="text-white text-lg font-medium hover:text-blue-300 transition duration-300 ease-in-out"
+            >
+              About Me
             </Link>
           </li>
           <li>
-            <Link href="/#projects" className="text-white hover:text-gray-400">
+            <Link
+              href="/#projects"
+              className="text-white text-lg font-medium hover:text-blue-300 transition duration-300 ease-in-out"
+            >
               Projects
             </Link>
           </li>
           <li>
-            <Link href="/#contact" className="text-white hover:text-gray-400">
+            <Link
+              href="/#contact"
+              className="text-white text-lg font-medium hover:text-blue-300 transition duration-300 ease-in-out"
+            >
               Contact
             </Link>
           </li>
